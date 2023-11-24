7a2226e60201e0535977134444d3fbca7531de8d
brenda <bbritob8@gmail.com>
2023-11-24 15:30:47 -0300

    incluindo classe player

---
+++ Player.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Jogador
+{
+    public string Nome { get; set; }
+    public string Nacionalidade { get; set; }
+    public int Idade { get; set; }
+    public string Posicao { get; set; }
+
+    public Jogador(string nome, string nacionalidade, int idade, string posicao)
+    {
+        Nome = nome;
+        Nacionalidade = nacionalidade;
+        Idade = idade;
+        Posicao = posicao;
+    }
+}
