6198952b74b260e51f485c09e85ebaa1847ae3ed
brenda <bbritob8@gmail.com>
2023-11-24 16:03:05 -0300

    implementando lógica

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program 
+{
+    static void Main (string[] args)
+    {
+        string NomeEntrada = Console.ReadLine();
+
+        int IdadeEntrada = Convert.ToInt32(Console.ReadLine());
+
+        string NacionalidadeEntrada = Console.ReadLine();
+
+        string PosicaoEntrada = Console.ReadLine();
+
+        Player player = new Player(NomeEntrada, NacionalidadeEntrada, IdadeEntrada, PosicaoEntrada);
+
+        string Nome = player.GetNome();
+        int Idade = player.GetIdade();
+        string Nacionalidade = player.GetNacionalidade();
+        string Posicao = player.GetPosicao();
+
+        Console.WriteLine($"Jogador criado!\n{Nome}\n{Nacionalidade}\n{Idade}\n Posição: {Posicao}");
+    }
+}
