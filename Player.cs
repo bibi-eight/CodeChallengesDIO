455552038ecfa541f2a2bf89ac18a65e650bfeac
brenda <bbritob8@gmail.com>
2023-11-24 16:02:55 -0300

    InNcluindo gets

--- Player.cs
+++ Player.cs
@@ -1,17 +1,38 @@
 using System;
 
-class Jogador
+class Player
 {
     public string Nome { get; set; }
     public string Nacionalidade { get; set; }
     public int Idade { get; set; }
     public string Posicao { get; set; }
 
-    public Jogador(string nome, string nacionalidade, int idade, string posicao)
+    public Player(string nome, string nacionalidade, int idade, string posicao)
     {
         Nome = nome;
         Nacionalidade = nacionalidade;
         Idade = idade;
         Posicao = posicao;
     }
+
+    public string GetNome()
+    {
+        return Nome;
+    }
+    
+    public int GetIdade()
+    {
+        return Idade;
+    }
+    
+    public string GetNacionalidade()
+    {
+        return Nacionalidade;
+    }
+    
+    public string GetPosicao()
+    {
+        return Posicao;
+    }
+
 }
